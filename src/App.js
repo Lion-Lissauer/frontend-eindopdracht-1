4046ab7b8543a0e9ed7cc1e78b2db89d4682d9d6
Lion-Lissauer <84527749+Lion-Lissauer@users.noreply.github.com>
2021-11-10 23:25:00 +0100

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState, useEffect} from "react";
-import Rijksmuseum from "./components/Rijksmuseum";
+import Rijksmuseum from "./components/Rijksmuseum/Rijksmuseum";
 import axios from "axios";
 import './App.css';
 
